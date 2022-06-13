f1a7174a2770748ac89caa4ee0f7e0bd896efd34
oddcode <oddcode@naver.com>
2022-06-13 18:50:48 +0900

    신규 아이디 추천 코드리뷰

--- programmers/level 1/3. 신규 아이디 추천.js
+++ programmers/level 1/3. 신규 아이디 추천.js
@@ -1,38 +1,24 @@
+/* 
+1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
+2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
+3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
+4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
+     만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
+7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
+*/
+
+//체이닝은 바로 지금과 같은 상황, 그러니까 바로 앞의 처리를 받아와서 다시 처리하고 처리하고 하는 경우를 위해 존재하는 것. 지금 안쓰면 언제쓰냐 도대체
 function solution(new_id) {
-  const regExp = /[\{\}\[\]\/?,;:|\)*~!^\+<>@\#$%&\\\=\(\'\"]/g;
-  const step1 = new_id.toLowerCase();
-  const step2 = step1.replace(regExp, "");
-  let step3 = step2.replace(/\.+/g, ".");
-  let step4;
-  if (step3.startsWith(".")) {
-    step3 = step3.substring(1);
-  }
-  if (step3.endsWith(".")) {
-    step4 = step3.substring(0, step3.length - 1);
-  } else {
-    step4 = step3;
-  }
-  let step5;
-  if (step4 == "") {
-    step5 = "a";
-  } else {
-    step5 = step4;
-  }
-  let step6;
-  if (step5.length >= 16) {
-    step6 = step5.slice(0, 15);
-    if (step6[step6.length - 1] === ".") {
-      step6 = step6.substring(0, step6.length - 1);
-    }
-  } else {
-    step6 = step5;
-  }
-  if (step6.length === 1) {
-    step6 = step6 + step6;
-  }
-  if (step6.length === 2) {
-    step6 = step6 + step6[1];
-  }
-  var answer = step6;
-  return answer;
+  const answer = new_id
+    .toLowerCase()
+    .replace(/[^\w-_.]/g, "")
+    .replace(/\.+/g, ".")
+    .replace(/^\.|\.$/g, "")
+    .replace(/^$/, "a")
+    .slice(0, 15)
+    .replace(/\.$/g, "");
+  const len = answer.length;
+  return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
 }
